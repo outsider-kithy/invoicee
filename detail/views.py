084c106b31699bb9df158ba6115c1b9ed739cd0d
outsider <outsider@outsidernoMacBook-Air.local>
2026-04-07 15:04:34 +0900

    detailページに更新用フォームを設置

--- detail/views.py
+++ detail/views.py
@@ -1,8 +1,7 @@
-from flask import render_template,request,Blueprint
+from flask import render_template,request,Blueprint,redirect,url_for
 from flask_login import login_required
-from models import session,Job,Client,Agency,Project,Tool,Account,Work,Content,Tax
-
-from helper import writeExcel
+from models import session,Job,Work,Content
+from sqlalchemy import func
 
 detail = Blueprint(
     "detail",
@@ -20,15 +19,58 @@ def index():
                 .filter(Job.id == selectedJobId)
                 .first()
             )
-        #print(selectedJob)
 
         contents = (
             session.query(Content)
             .filter(Content.job_id == selectedJobId)
             .all()
         )
-        #print(contents)
+        
+        works = session.query(Work).all()
 
-        return render_template("detail/index.html", selectedJob=selectedJob, contents=contents)
+        total_price = (
+            session.query(func.sum(Work.price))
+            .join(Content, Content.work_id == Work.id)
+            .filter(Content.job_id == selectedJobId)
+            .scalar()
+)
+        return render_template(
+            "detail/index.html", 
+            selectedJob=selectedJob, 
+            contents=contents, 
+            works=works,
+            total_price = total_price
+        )
+    
     elif request.method == "POST":
-        return render_template("detail/index.html", selectedJobId=selectedJobId)
+        update_job_id = request.form['update_job_id']
+        update_works = request.form.getlist("update_work[]")
+        update_contents = request.form.getlist("update_content[]")
+
+        # Jobに紐づく既存のContent一覧を取得
+        contents = (
+            session.query(Content)
+            .filter(Content.job_id == update_job_id)
+            .order_by(Content.id)
+            .all()
+        )
+
+        for content_record, work_type, work_content in zip(
+            contents,
+            update_works,
+            update_contents,
+        ):
+            # 選択されたwork_typeに対応するWorkレコードを取得
+            work_record = (
+                session.query(Work)
+                .filter(Work.work_type == work_type)
+                .first()
+            )
+
+            # Contentテーブル更新
+            content_record.work_id = work_record.id
+            content_record.work_content = work_content
+
+            session.commit()
+
+        return redirect(url_for('update.index'))
